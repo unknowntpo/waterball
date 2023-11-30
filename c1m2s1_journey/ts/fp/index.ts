8f5743162c3c1b9d2c687339b9c7392633e7be83
unknowntpo <e850506@gmail.com>
2023-11-30 09:22:45 +0800

    journey:fp: use introsection type to handle different Scene impl

--- c1m2s1_journey/ts/fp/index.ts
+++ c1m2s1_journey/ts/fp/index.ts
@@ -1,4 +1,8 @@
-interface Student {
+import { on } from "events"
+import { execPath } from "process"
+import { Context } from "vm"
+
+type Student = {
     account: string,
     password: string,
     level: number,
@@ -15,17 +19,79 @@ const levelUp(student: Student): Student => ({
     level: student.level++,
 })
 
-type MissionState = "ON_GOING" | "COMPLETED"
-
-interface MissionCarryOn {
-    state: MissionState
+enum MissionState {
+    OnGoing,
+    Completed,
 }
 
-const carryOn(student: Student, mission: Mission): MissionCarryOn => ({
-    state: "ON_GOING",
+type Mission = {
+    name: string,
+    number: number
+    scenes: Array<Scene>
+}
+
+type Chapter = {
+    name: string,
+    number: number
+}
+
+type Scene = {
+    name: string,
+    number: number,
+    expAward: number
+    typ: SceneType,
+}
+
+enum SceneType {
+    Video,
+    Content
+}
+
+type VideoScene = Scene & { typ: SceneType.Video }
+type ContentScene = Scene & { typ: SceneType.Content }
+
+type Challenge = {
+    name: string,
+    number: number
+}
+
+// type MissionState = "ON_GOING" | "COMPLETED"
+
+type MissionCarryOn = {
+    state: MissionState
+    mission: Mission
+}
+
+const carryOn = (student: Student, mission: Mission): MissionCarryOn => ({
+    mission: mission,
+    state: MissionState.OnGoing,
 })
 
-const completeMission(missionCarryOn: MissionCarryOn) => missionCarryOn {
-    // TODO: calculateAward
+type ExpAwardable = Mission | Scene
 
+type expCalculateFn = (target: ExpAwardable) => number;
+
+const calculateExpAward = (target: ExpAwardable, fn: expCalculateFn): number => {
+    return fn(target)
 }
+
+const calculateVideoSceneExpAward = (target: VideoScene): number => Math.floor(target.expAward * 1.5)
+const calculateContentSceneExpAward = (target: ContentScene): number => Math.floor(target.expAward * 1.1)
+
+const calculateSceneExpAward = (target: Scene): number => {
+    switch (target.typ) {
+        case (SceneType.Video):
+            return calculateVideoSceneExpAward(target as VideoScene)
+        case (SceneType.Content):
+            return calculateContentSceneExpAward(target as ContentScene)
+    }
+}
+
+const calculateMissionExpAward = (target: Mission): number => {
+    return target.scenes.map((scene: Scene): number => calculateSceneExpAward(scene)).reduce((acc: number, award: number) => acc + award), 0)
+}
+
+const completeMissionCarryOn = (missionCarryOn: MissionCarryOn): MissionCarryOn => ({
+    ...missionCarryOn,
+    state: MissionState.Completed
+})
