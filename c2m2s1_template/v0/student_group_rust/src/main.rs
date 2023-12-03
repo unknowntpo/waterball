0768f4624d9fd6f5f9c2744682b23d58d519e18d
unknowntpo <e850506@gmail.com>
2023-12-03 18:03:15 +0800

    get student data from file

--- c2m2s1_template/v0/student_group_rust/src/main.rs
+++ c2m2s1_template/v0/student_group_rust/src/main.rs
@@ -1,10 +1,13 @@
 mod group;
 mod student;
 
+use std::fs::File;
+use std::io::{Read, Result as IOResult};
 use student::Student;
 
 fn main() {
-    let students = parse_student_data("student.data").unwrap();
+    let content = read_file("student.data").unwrap();
+    let students = parse_student_data(content).unwrap();
 
     // first cut
     let groupStrategy = group::LangBasedGroupStrategy::new();
@@ -14,45 +17,57 @@ fn main() {
 
 type ParseErr = String;
 
-fn parse_student_data(s: &str) -> Result<Vec<Student>, ParseErr> {
+fn read_file(file_name: &str) -> IOResult<String> {
+    let mut f = File::open(file_name)?;
+    let mut content = String::new();
+    f.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+fn parse_student_data(s: String) -> Result<Vec<Student>, ParseErr> {
     // split
-    let _s = "Spencer 14y C# 學生 [10 11 15 18 19]";
+    // let _s = "Spencer 14y C# 學生 [10 11 15 18 19]";
 
-    // Find the indices of the square brackets
-    let start_bracket = _s.find('[').unwrap();
-    let end_bracket = _s.find(']').unwrap();
+    let lines: Vec<_> = s.lines().collect();
+    let mut students: Vec<Student> = Vec::with_capacity(lines.len());
+    for _s in lines {
+        // Find the indices of the square brackets
+        let start_bracket = _s.find('[').unwrap();
+        let end_bracket = _s.find(']').unwrap();
 
-    // Extract the part inside the brackets and format it
-    let numbers = &_s[start_bracket..=end_bracket];
-    let formatted_numbers = numbers.replace(" ", ",");
+        // Extract the part inside the brackets and format it
+        let numbers = &_s[start_bracket..=end_bracket];
+        let formatted_numbers = numbers.replace(" ", ",");
 
-    // Split the rest of the string by whitespace and collect into a vector
-    let mut tokens: Vec<&str> = _s[..start_bracket].split_whitespace().collect();
+        // Split the rest of the string by whitespace and collect into a vector
+        let mut tokens: Vec<&str> = _s[..start_bracket].split_whitespace().collect();
 
-    // Append the formatted number string
-    tokens.push(&formatted_numbers);
+        // Append the formatted number string
+        tokens.push(&formatted_numbers);
 
-    // let tokens: Vec<&str> = _s.split("[").collect();
-    if tokens.len() != 5 {
-        return Err(format!("wrong number of arguments in {:?}", tokens));
+        // let tokens: Vec<&str> = _s.split("[").collect();
+        if tokens.len() != 5 {
+            return Err(format!("wrong number of arguments in {:?}", tokens));
+        }
+
+        println!("tokens: {:?}", tokens);
+
+        students.push(Student::new(
+            tokens[0],
+            tokens[1].trim_matches(|c| c == 'y').parse::<i32>().unwrap(),
+            tokens[2],
+            tokens[3],
+            // trim [ and ]
+            tokens[4]
+                .to_string()
+                .trim_matches(|c| c == '[' || c == ']')
+                .split(",")
+                .map(|num_str| num_str.trim().parse::<i8>())
+                .collect::<Result<Vec<i8>, _>>()
+                .unwrap(),
+        ));
     }
-
-    println!("tokens: {:?}", tokens);
-
-    Ok(vec![Student::new(
-        tokens[0],
-        tokens[1].trim_matches(|c| c == 'y').parse::<i32>().unwrap(),
-        tokens[2],
-        tokens[3],
-        // trim [ and ]
-        tokens[4]
-            .to_string()
-            .trim_matches(|c| c == '[' || c == ']')
-            .split(",")
-            .map(|num_str| num_str.trim().parse::<i8>())
-            .collect::<Result<Vec<i8>, _>>()
-            .unwrap(),
-    )])
+    Ok(students)
 }
 
 // fn parse_string_vec<T: neon>(s: &str) -> Vec<i32> {}
