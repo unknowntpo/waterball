ef802e5715fa102c497980dd9d6ff9fb2a65dd61
unknowntpo <e850506@gmail.com>
2024-03-10 08:29:31 +0800

    add dummy impl of DistanceBasedMatchingStrategy

--- matching/app/src/main/java/matching/DistanceBasedMatchingStrategy.java
+++ matching/app/src/main/java/matching/DistanceBasedMatchingStrategy.java
@@ -1,6 +1,12 @@
 package matching;
 
 import java.util.List;
+import java.util.Random;
+
+import org.checkerframework.common.value.qual.ArrayLen;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DistanceBasedMatchingStrategy extends MatchingStrategy {
     private Modifier modifier;
@@ -8,7 +14,27 @@ public class DistanceBasedMatchingStrategy extends MatchingStrategy {
     @Override
     List<Pair> doMatch(List<Individual> individuals) {
         // TODO Auto-generated method stub
-        return null;
+        HashMap<Integer, Boolean> selected = new HashMap<Integer, Boolean>();
+        Random rand = new Random();
+        ArrayList<Pair> pairs = new ArrayList<Pair>();
+        while (selected.size() < individuals.size()) {
+            Individual left = null;
+            Individual right = null;
+            int leftIdx = rand.nextInt(individuals.size());
+            while (!selected.containsKey(leftIdx)) {
+                left = individuals.get(leftIdx);
+                selected.put(leftIdx, true);
+            }
+            int rightIdx = rand.nextInt(individuals.size());
+            while (!selected.containsKey(rightIdx)) {
+                right = individuals.get(rightIdx);
+                selected.put(rightIdx, true);
+            }
+            pairs.add(new Pair(left, right));
+        }
+        // A . B C D
+        // pick random
+        return pairs;
     }
 
     DistanceBasedMatchingStrategy(Modifier modifier) {
--- matching/app/src/main/java/matching/Pair.java
+++ matching/app/src/main/java/matching/Pair.java
@@ -3,4 +3,9 @@ package matching;
 public class Pair {
     private Individual left;
     private Individual right;
+
+    public Pair(Individual left, Individual right) {
+        this.left = left;
+        this.right = right;
+    }
 }
