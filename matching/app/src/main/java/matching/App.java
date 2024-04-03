fbf16da4654b930d8a9b2217876f56beeb10b0e6
unknowntpo <e850506@gmail.com>
2024-04-04 07:57:55 +0800

    feat(matching:App): add main method

--- matching/app/src/main/java/matching/App.java
+++ matching/app/src/main/java/matching/App.java
@@ -3,12 +3,37 @@
  */
 package matching;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+public class App {
+	private static final Logger LOGGER = Logger.getLogger(DistanceBasedMatchingStrategy.class.getClass().getName());
+
+	public String getGreeting() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		List<Individual> individuals = new ArrayList<Individual>();
+
+		individuals.add(new Individual(1, Individual.Gender.MALE, 25, "Intro of individual 1", "Habits of individual 1",
+				"(1,3)"));
+		individuals.add(new Individual(2, Individual.Gender.FEMALE, 30, "Intro of individual 2",
+				"Habits of individual 2", "(2,4)"));
+		individuals.add(new Individual(3, Individual.Gender.MALE, 22, "Intro of individual 3", "Habits of individual 3",
+				"(4,5)"));
+		individuals.add(new Individual(4, Individual.Gender.FEMALE, 28, "Intro of individual 4",
+				"Habits of individual 4", "(5,6)"));
+
+		// match with distance (reversed)
+		var distanceBasedStrategy = new DistanceBasedMatchingStrategy(new ReverseModifier());
+		var pairs = distanceBasedStrategy.doMatch(individuals);
+
+		LOGGER.info(pairs.toString());
+
+		var habitBasedStrategy = new HabitBasedMatchingStrategy();
+		pairs = habitBasedStrategy.doMatch(individuals);
+		LOGGER.info(pairs.toString());
+	}
 }
