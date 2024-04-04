500209dc1c07eb2b69bccaae786aa007c45aeddf
unknowntpo <e850506@gmail.com>
2024-04-04 08:11:08 +0800

    feat(matching): tweak HabitBasedMatchingStrategyTest

--- matching/app/src/test/java/matching/HabitBasedMatchingStrategyTest.java
+++ matching/app/src/test/java/matching/HabitBasedMatchingStrategyTest.java
@@ -12,25 +12,33 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class HabitBasedMatchingStrategyTest {
-    private MatchingStrategy strategy;
+	private MatchingStrategy strategy;
 
-    @Test
-    void shouldMatch() {
-        List<Individual> individuals = new ArrayList<>();
+	@Test
+	void shouldMatch() {
+		List<Individual> individuals = new ArrayList<>();
 
-        individuals.add(new Individual(1, Individual.Gender.MALE, 25, "Intro of individual 1", "Habits of individual 1",
-                "Coor 1"));
-        individuals.add(new Individual(2, Individual.Gender.FEMALE, 30, "Intro of individual 2",
-                "Habits of individual 2", "Coor 2"));
-        individuals.add(new Individual(3, Individual.Gender.MALE, 22, "Intro of individual 3", "Habits of individual 3",
-                "Coor 3"));
-        individuals.add(new Individual(4, Individual.Gender.FEMALE, 28, "Intro of individual 4",
-                "Habits of individual 4", "Coor 4"));
+		individuals.add(new Individual(1, Individual.Gender.MALE, 25, "Intro of individual 1",
+				"mountain_climbing", "Coor 1"));
+		individuals.add(new Individual(2, Individual.Gender.FEMALE, 30, "Intro of individual 2",
+				"game,sleep,coffee", "Coor 2"));
+		individuals
+				.add(new Individual(3, Individual.Gender.MALE, 22, "Intro of individual 3", "game,sleep,coffee,workout",
+						"Coor3"));
+		individuals.add(new Individual(4, Individual.Gender.FEMALE, 28, "Intro of individual 4",
+				"workout,biking,jogging", "Coor 4"));
 
-        this.strategy = new HabitBasedMatchingStrategy();
-        assertTrue(this.strategy.match(individuals).size() / 2 == individuals.size(),
-                "length of pairs should be individuals.size() / 2");
-        // assertEquals("Hello", this.strategy.match(individuals), "should be the
-        // same");
-    }
+		this.strategy = new HabitBasedMatchingStrategy();
+		var pairs = this.strategy.match(individuals);
+		assertTrue(pairs.size() / 2 == individuals.size(),
+				"length of pairs should be individuals.size() / 2");
+
+		// assertTrue(this.strategy.match(individuals).size() / 2 == individuals.size(),
+		// "length of pairs should be individuals.size() / 2");
+		assertTrue(pairs.contains(new Pair(individuals.get(0), individuals.get(3))));
+		assertTrue(pairs.contains(new Pair(individuals.get(1), individuals.get(2))));
+
+		// assertEquals("Hello", this.strategy.match(individuals), "should be the
+		// same");
+	}
 }
