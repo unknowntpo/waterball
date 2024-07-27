951fc736e800a245c8ffb026deaa6b2e158b6498
unknowntpo <e850506@gmail.com>
2024-07-27 18:31:55 +0800

    feat(collision): fix comp error

--- collision/src/world.rs
+++ collision/src/world.rs
@@ -1,9 +1,9 @@
 type SpriteId = i32;
 
 pub trait Sprite {
-    fn getId() -> SpriteId;
-    fn getType() -> SpriteType;
-    fn register_handler(&self) -> dyn Handler;
+    fn get_id(&self) -> SpriteId;
+    fn get_type(&self) -> SpriteType;
+    fn register_handler(&self) -> Box<dyn Handler>;
 }
 
 enum SpriteType {}
@@ -19,23 +19,32 @@ trait Handler {
 }
 
 impl World {
-    pub fn new(sprites: Vec<Sprite>, axis: Vec<Vec<SpriteId>>) -> Self {
-        Self { sprites, axis }
+    pub fn new(sprites: Vec<Box<dyn Sprite>>, axis: Vec<Vec<SpriteId>>) -> Self {
+        let handlers = sprites
+            .iter()
+            .map(|sprite| sprite.register_handler())
+            .collect();
+
+        Self {
+            sprites,
+            axis,
+            handlers,
+        }
     }
 
     pub fn run(&self) {
         loop {
-            let (base, target) = self.getInput();
-            self.moveTo(base, target);
+            let (base, target) = self.get_input();
+            self.move_to(base, target);
         }
     }
 
-    fn getInput(&self) -> (i32, i32) {
+    fn get_input(&self) -> (i32, i32) {
         return (1, 2);
     }
 
-    fn moveTo(&self, base: i32, target: i32) {
-        for handler in self.handlers {
+    fn move_to(&self, base: i32, target: i32) {
+        for handler in &self.handlers {
             if handler.handle(base, target) {
                 break;
             }
